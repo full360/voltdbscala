e8ae48c0151e6554ca7e7699f0aa0d1f8feb4627
Diorman Colmenares <dextrin.diorman@gmail.com>
2016-11-08 20:14:09 -0500

    Add method to validate status of client responses

--- src/main/scala/com/full360/voltdbscala/ClientResponseUtils.scala
+++ src/main/scala/com/full360/voltdbscala/ClientResponseUtils.scala
@@ -11,30 +11,51 @@ object ClientResponseUtils {
    *
    * @param clientResponse the ClientResponse to process results from
    * @param index the index of the results array to parse
+   * @param validateStatus flag for checking the status of the client response before doing any other operation
    * @param f a function responsible for parsing a single row
    * @tparam T the type the rows are parsed to
    * @return an Sequence of given type containing the parsed rows
    */
-  def mapClientResponseResult[T](clientResponse: ClientResponse, index: Int)(f: VoltTableRow ⇒ T): Seq[T] =
+  def mapClientResponseResult[T](clientResponse: ClientResponse, index: Int, validateStatus: Boolean = true)(f: VoltTableRow ⇒ T): Seq[T] = {
+    if (validateStatus) validateClientResponseStatus(clientResponse)
     VoltTableUtils.mapVoltTable[T](clientResponse.getResults()(index))(f)
+  }
 
   /**
    * Parses just the first row of the results.
    *
    * @param clientResponse the ClientResponse to process results from
    * @param index the index of the results array to parse
+   * @param validateStatus flag for checking the status of the client response before doing any other operation
    * @param f a function responsible for parsing a single row
    * @tparam T the type the row is parsed to
    * @return an Option of given type containing the first row or None if the results are empty
    */
-  def mapFirstRowFromClientResponseResult[T](clientResponse: ClientResponse, index: Int)(f: VoltTableRow ⇒ T): Option[T] =
+  def mapFirstRowFromClientResponseResult[T](clientResponse: ClientResponse, index: Int, validateStatus: Boolean = true)(f: VoltTableRow ⇒ T): Option[T] = {
+    if (validateStatus) validateClientResponseStatus(clientResponse)
     VoltTableUtils.mapFirstRowFromVoltTable[T](clientResponse.getResults()(index))(f)
+  }
+
+  /**
+   * Checks that the status of the client response is SUCCESS. If not, it throws an exception
+   *
+   * @param clientResponse client response instance to be evaluated
+   * @return the same client response passed in when its status is equal to <code>ClientResponse.SUCCESS</code>
+   */
+  def validateClientResponseStatus(clientResponse: ClientResponse): ClientResponse = clientResponse.getStatus match {
+    case ClientResponse.SUCCESS ⇒ clientResponse
+    case _                      ⇒ throw ClientResponseStatusException(clientResponse)
+  }
 
   implicit class MapMethodSupport(clientResponse: ClientResponse) {
-    def map[T](index: Int)(f: VoltTableRow ⇒ T): Seq[T] =
-      ClientResponseUtils.mapClientResponseResult(clientResponse, index)(f)
+    def map[T](index: Int, validateStatus: Boolean = true)(f: VoltTableRow ⇒ T): Seq[T] =
+      ClientResponseUtils.mapClientResponseResult(clientResponse, index, validateStatus)(f)
 
-    def mapFirstRow[T](index: Int)(f: VoltTableRow ⇒ T): Option[T] =
-      ClientResponseUtils.mapFirstRowFromClientResponseResult(clientResponse, index)(f)
+    def mapFirstRow[T](index: Int, validateStatus: Boolean = true)(f: VoltTableRow ⇒ T): Option[T] =
+      ClientResponseUtils.mapFirstRowFromClientResponseResult(clientResponse, index, validateStatus)(f)
+  }
+
+  implicit class ValidateStatusMethodSupport(clientResponse: ClientResponse) {
+    def validateStatus: ClientResponse = validateClientResponseStatus(clientResponse)
   }
 }
--- src/test/scala/com/full360/voltdbscala/ClientResponseUtilsTest.scala
+++ src/test/scala/com/full360/voltdbscala/ClientResponseUtilsTest.scala
@@ -8,9 +8,11 @@ import org.voltdb.client.{ClientResponse, TestClientResponse}
 class ClientResponseUtilsTest extends WordSpec with Matchers {
   case class TestObj(col1: String, col2: String)
 
-  def createClientResponse(nRows: Int): ClientResponse = new TestClientResponse {
+  def createClientResponse(nRows: Int, status: Byte = ClientResponse.SUCCESS, statusString: String = "SUCCESS"): ClientResponse = new TestClientResponse {
     val f = (row: Int, col: Int) ⇒ s"r${row}:c${col}"
     override def getResults: Array[VoltTable] = Array(VoltTableBuilder.build(nRows, VoltType.STRING, VoltType.STRING)(f))
+    override def getStatus: Byte = status
+    override def getStatusString: String = statusString
   }
 
   def parseTestRow(row: VoltTableRow) = TestObj(row.getString(0), row.getString(1))
@@ -26,6 +28,14 @@ class ClientResponseUtilsTest extends WordSpec with Matchers {
     )
   }
 
+  "ClientResponseUtils.mapClientResponseResult throws a ClientResponseStatusException if client response is not valid" in {
+    val response = createClientResponse(3, ClientResponse.RESPONSE_UNKNOWN, "RESPONSE_UNKNOWN")
+
+    intercept[ClientResponseStatusException] {
+      ClientResponseUtils.mapClientResponseResult(response, 0)(parseTestRow)
+    }
+  }
+
   "ClientResponseUtils.mapFirstRowFromClientResponseResult maps first row into Some[T]" in {
     val response = createClientResponse(3)
     val result = ClientResponseUtils.mapFirstRowFromClientResponseResult(response, 0)(parseTestRow)
@@ -34,6 +44,14 @@ class ClientResponseUtilsTest extends WordSpec with Matchers {
   }
 
   "ClientResponseUtils.mapFirstRowFromClientResponseResult maps first row into None if no records where found" in {
+    val response = createClientResponse(1, ClientResponse.CONNECTION_TIMEOUT, "CONNECTION_TIMEOUT")
+
+    intercept[ClientResponseStatusException] {
+      ClientResponseUtils.mapFirstRowFromClientResponseResult(response, 0)(parseTestRow)
+    }
+  }
+
+  "ClientResponseUtils.mapFirstRowFromClientResponseResult throws a ClientResponseStatusException if client response is not valid" in {
     val response = createClientResponse(0)
     val result = ClientResponseUtils.mapFirstRowFromClientResponseResult(response, 0)(parseTestRow)
 
@@ -47,10 +65,13 @@ class ClientResponseUtilsTest extends WordSpec with Matchers {
     response.map(0)(parseTestRow) shouldBe Seq(TestObj("r1:c1", "r1:c2"))
   }
 
-  "ClientResponseUtils.MapMethodSupport adds 'mapFirstRow' method to ClientResponse instances" in {
-    import ClientResponseUtils.MapMethodSupport
+  "ClientResponseUtils.ValidateStatusMethodSupport adds 'validateStatus' method to ClientResponse instances" in {
+    import ClientResponseUtils.ValidateStatusMethodSupport
 
-    val response = createClientResponse(1)
-    response.mapFirstRow(0)(parseTestRow) shouldBe Some(TestObj("r1:c1", "r1:c2"))
+    val response = createClientResponse(0, ClientResponse.CONNECTION_LOST, "CONNECTION_LOST")
+
+    intercept[ClientResponseStatusException] {
+      response.validateStatus
+    }
   }
 }
