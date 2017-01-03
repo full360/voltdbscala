495e15ba30f3ae3f96d86cad98055156b5017de9
Diorman Colmenares <dextrin.diorman@gmail.com>
2017-01-03 15:21:29 -0400

    Upgrade VoltDB dependency to 6.8

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -33,7 +33,7 @@ object ProjectSettings {
   ) ++ formatSettings
 
   lazy val libraryDependencies_ = Seq(
-    "org.voltdb"              % "voltdbclient"   % "6.6" % "provided",
+    "org.voltdb"              % "voltdbclient"   % "6.8" % "provided",
     "org.scalatest"          %% "scalatest"      % "3.0.0" % "test",
     "org.mockito"             % "mockito-all"    % "1.9.5" % "test"
   )
